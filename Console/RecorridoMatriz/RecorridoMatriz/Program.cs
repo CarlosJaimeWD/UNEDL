189957875e799d6dd7a056176a54257713066f44
Carlos Jaime <carlosjaimewd@gmail.com>
2019-03-18 21:12:43 -0600

    Inicial carpeta C#

---
+++ Console/RecorridoMatriz/RecorridoMatriz/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecorridoMatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[][] array = null;
+
+            for (int y = 0; y <= 0; y++)
+            {
+
+            }
+        }
+    }
+}
