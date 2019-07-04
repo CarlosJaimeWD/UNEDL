c805048f0917037408149b69e28685ef23edb498
Carlos Jaime Mac <carlosjaimewd@gmail.com>
2019-07-04 09:59:37 -0500

    df

---
+++ valorMayor/src/valormayor/ValorMayor.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package valormayor;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author carlosjaime
+ */
+public class ValorMayor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int tamaño = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el tamaño del arreglo: "));
+        int operaciones = Integer.parseInt(JOptionPane.showInputDialog("Ingresa la cantidad de operaciones: "));
+        
+        ArrayList<Integer> values = new ArrayList<>();
+        
+        for (int i = 0; i < operaciones; i++) {
+            
+        }
+    }
+    
+}
